56c0f079c353f8186e3cd513babcdb597131fa25
Rostislav-Dusnii <rostislav.dusnii@student.ucll.be>
2025-12-19 10:24:06 +0100

    remove data

--- pages/shared-circles/[shareId].tsx
+++ pages/shared-circles/[shareId].tsx
@@ -539,12 +539,13 @@ export default function SharedCirclesPage() {
 }
 
 
+import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { GetStaticProps } from "next";
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale ?? 'en', ['common'])),
+      ...(await serverSideTranslations(locale ?? "en", ["common"])),
     },
   };
 };
